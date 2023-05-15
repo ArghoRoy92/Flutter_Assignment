afb36bc51fc386024962f568fd29e151318d2af7
Ar G Ho <41030992+ArghoRoy92@users.noreply.github.com>
2023-05-15 10:04:30 +0600

    Update Assignment_3.dart

--- Assignment_3.dart
+++ Assignment_3.dart
@@ -41,21 +41,20 @@ class Car {
 }
 
 void main() {
-  Car car1 = Car("Toyota", "Camry", 2010);
+  Car car1 = Car("Mitsubisi", "Lancer", 2010);
   car1.drive(10000);
 
-  Car car2 = Car("Honda", "Civic", 2015);
+  Car car2 = Car("Honda", "CHR", 2015);
   car2.drive(5000);
 
-  Car car3 = Car("Ford", "Mustang", 2020);
+  Car car3 = Car("Tata", "Nano", 2020);
   car3.drive(2000);
 
+  print("The Specification of total ${Car.numberOfCars} cars are:");
   print(
-      "Car 1: ${car1.getBrand()} ${car1.getModel()} ${car1.getYear()} ${car1.getMilesDriven()} miles driven, ${car1.getAge()} years old");
+      " ${car1.getBrand()} ${car1.getModel()} ${car1.getYear()} ${car1.getMilesDriven()} miles driven, ${car1.getAge()} years");
   print(
-      "Car 2: ${car2.getBrand()} ${car2.getModel()} ${car2.getYear()} ${car2.getMilesDriven()} miles driven, ${car2.getAge()} years old");
+      " ${car2.getBrand()} ${car2.getModel()} ${car2.getYear()} ${car2.getMilesDriven()} miles driven, ${car2.getAge()} years");
   print(
-      "Car 3: ${car3.getBrand()} ${car3.getModel()} ${car3.getYear()} ${car3.getMilesDriven()} miles driven, ${car3.getAge()} years old");
-
-  print("Total number of cars created: ${Car.numberOfCars}");
+      " ${car3.getBrand()} ${car3.getModel()} ${car3.getYear()} ${car3.getMilesDriven()} miles driven, ${car3.getAge()} years");
 }
